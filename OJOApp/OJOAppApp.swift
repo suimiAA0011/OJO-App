904835ff5f2c1c645506d5f8b91ac269009dc894
ShahadQ <shqadi22@twq.idserve.net>
2023-02-07 09:40:32 +0300

    Initial Commit

---
+++ OJOApp/OJOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OJOAppApp.swift
+//  OJOApp
+//
+//  Created by ShahadQadi on 16/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct OJOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
