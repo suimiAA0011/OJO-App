7b1c170dc3ed0603ae9cc0c345f1f5fb363e24be
ShahadQ <shqadi22@twq.idserve.net>
2023-02-07 10:24:06 +0300

    OGOApp

---
+++ OJOApp/MainPage.swift
@@ -0,0 +1,20 @@
+//
+//  MainPage.swift
+//  OJOApp
+//
+//  Created by ShahadQadi on 16/07/1444 AH.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainPage_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPage()
+    }
+}
---
+++ OJOApp/Onboarding.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding.swift
+//  OJOApp
+//
+//  Created by ShahadQadi on 16/07/1444 AH.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
